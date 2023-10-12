1f85c30dc0e4070ad4a7a975efbef754cf3b7ad4
timsydrame <fatimdrame111@gmail.com>
2023-10-12 15:38:16 +0200

    Css ajustement

--- src/Form/CandidateResponseType.php
+++ src/Form/CandidateResponseType.php
@@ -7,14 +7,23 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class CandidateResponseType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('enoncer')
-            ->add('imageResponse')
+            ->add('enoncer', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Entrez votre énoncé de reponse',
+                ],
+            ])
+            // ->add('imageResponse', TextType::class, [
+            //     'attr' => [
+            //         'placeholder' => 'Ajoutez le lien de l\'image ici',
+            //     ],
+            // ])
             ->add('iscorrect', CheckboxType::class, [
                 'label' => 'Est-ce la bonne réponse?',
                 'required' => false,
